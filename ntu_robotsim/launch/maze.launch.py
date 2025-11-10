f07af6537a5a4e3b7f89223bd59420d3eb52cf5b
kalhansb <kalhansandaru@gmail.com>
2025-11-10 16:53:20 +0000

    Fixed errors

--- ntu_robotsim/launch/maze.launch.py
+++ ntu_robotsim/launch/maze.launch.py
@@ -7,10 +7,10 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    ntu_hmrs_sim_share_dir = get_package_share_directory('ntu_hmrs_sim')
+    ntu_robotsim_share_dir = get_package_share_directory('ntu_robotsim')
     ros_gz_sim_share_dir = get_package_share_directory('ros_gz_sim')
 
-    gz_args = os.path.join(ntu_hmrs_sim_share_dir, 'worlds', 'maze', 'maze.sdf')
+    gz_args = os.path.join(ntu_robotsim_share_dir, 'worlds', 'maze', 'maze.sdf')
     gz_args += " -v 4"
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
